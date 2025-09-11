21ab2d0c171b127e9ff5a0c1481e6f7a2aaabb57
dani atienza <danichilling@gmail.com>
2025-09-11 16:48:50 +0100

    updated gallery and form options

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,19 +2,14 @@ import { Component } from '@angular/core';
 import { TattooGalleryComponent } from './tattoo-gallery/tattoo-gallery.component';
 import { TattooDesignOptions } from './tattoo-gallery';
 import { FormDetailsComponent } from './form-details/form-details.component';
-import { CalendarDetailsComponent } from './calendar-details/calendar-details.component';
 
 @Component({
   selector: 'app-root',
-  imports: [TattooGalleryComponent, FormDetailsComponent, CalendarDetailsComponent],
+  imports: [TattooGalleryComponent, FormDetailsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
 [x: string]: any;
   title = 'Dani\'s Tattoo Booking';
-
-  
-
-  
 }
--- src/app/calendar-details.ts
+++
@@ -1,4 +0,0 @@
-export interface CalendarDetails {
-  calendar: string;
-  calendarDateSelected: string;
-}
